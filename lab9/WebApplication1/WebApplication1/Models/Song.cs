34a0f6c25754c49e2edc608eea47ef42d44b7ec2
NadiraX <57359646+NadiraX@users.noreply.github.com>
2020-05-21 20:41:36 +0200

    30/69

---
+++ lab9/WebApplication1/WebApplication1/Models/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Song
+
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        
+    }
+}
